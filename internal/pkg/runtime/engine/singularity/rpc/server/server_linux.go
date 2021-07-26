77c0c744322974e45d3373f04f4a1f01502250d5
David Trudgian <david.trudgian@sylabs.io>
2021-07-26 11:34:57 -0500

    Split pkg/util/crypt key and device functionality
    `pkg/util/crypt` contained code that calls out to `cryptsetup`, which is
    found via routines that rely on a valid buildcfg (only present when
    SingularityCE is built via `mconfig`). This, in turn, prevents
    `pkg/build/types` being imported from other projects as `bundle.go`
    imports `pkg/util/crypt`.
    
    Move the device handling portions of the crypt code to
    `internal/pkg/util/crypt`.
    
    Keep the remaining key handling portions of the code, in
    `pkg/util/cryptkey`. These now have no internal or buildcfg
    dependencies.
    
    Fixes #181

--- internal/pkg/runtime/engine/singularity/rpc/server/server_linux.go
+++ internal/pkg/runtime/engine/singularity/rpc/server/server_linux.go
@@ -15,12 +15,12 @@ import (
 	"syscall"
 
 	args "github.com/sylabs/singularity/internal/pkg/runtime/engine/singularity/rpc"
+	"github.com/sylabs/singularity/internal/pkg/util/crypt"
 	"github.com/sylabs/singularity/internal/pkg/util/fs"
 	"github.com/sylabs/singularity/internal/pkg/util/mainthread"
 	"github.com/sylabs/singularity/internal/pkg/util/user"
 	"github.com/sylabs/singularity/pkg/sylog"
 	"github.com/sylabs/singularity/pkg/util/capabilities"
-	"github.com/sylabs/singularity/pkg/util/crypt"
 	"github.com/sylabs/singularity/pkg/util/loop"
 	"github.com/sylabs/singularity/pkg/util/namespaces"
 	"golang.org/x/sys/unix"
