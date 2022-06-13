9eccb4ade29cf3ac7aef0e3a554d10315250887c
vsoch <vsoch@users.noreply.github.com>
2022-06-13 15:27:46 -0600

    use bin.FindBin provided by singularity for truncate
    Signed-off-by: vsoch <vsoch@users.noreply.github.com>

--- internal/app/singularity/overlay_create.go
+++ internal/app/singularity/overlay_create.go
@@ -78,7 +78,7 @@ func findConvertCommand(overlaySparse bool) (string, error) {
 
 	// Sparse overlay requires truncate -s
 	if overlaySparse {
-		truncate, err := exec.LookPath(truncateBinary)
+		truncate, err := bin.FindBin(truncateBinary)
 		if err != nil {
 			return command, err
 		}
