344717d2878bdb5439657a3dcc103dd6ab3ab2b7
David Trudgian <dtrudg@sylabs.io>
2022-09-21 16:03:36 +0100

    fix: Adjust tests for new Setresuid/gid behavior
    Bringing in the newest x/sys/unix modifies unix.Setresuid/gid to be all
    thread, via Go 1.16 syscall changes.
    
    Go 1.16 uses or emulates libc behaviour for syscall.Setresuid/gid,
    making them all-thread. The new x/sys/unix now leverages
    syscall.Setresuid/gid.
    
    In sylog unit tests we previously had an unintuitive output check
    reflecting the prior thread-specific nature of the calls. We now see
    the correct unpriv UID in messages, as all threads were altered. Adjust
    the test to accommodate this.
    
    In the e2e tests we need to preserve per-thread escalation/drop for
    parallel tests started from the same process. Use some raw syscalls to
    achieve this.

--- internal/pkg/util/priv/priv_linux.go
+++ internal/pkg/util/priv/priv_linux.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -11,14 +11,18 @@ import (
 	"syscall"
 )
 
-// Escalate escalates thread privileges.
+// Escalate escalates privileges of the thread or process.
+// Since Go 1.16 syscall.Setresuid is an all-thread operation.
+// A runtime.LockOSThread operation remains for older versions of Go.
 func Escalate() error {
 	runtime.LockOSThread()
 	uid := os.Getuid()
 	return syscall.Setresuid(uid, 0, uid)
 }
 
-// Drop drops thread privileges.
+// Drop drops privileges of the thread or process.
+// Since Go 1.16 syscall.Setresuid is an all-thread operation.
+// A runtime.LockOSThread operation remains for older versions of Go.
 func Drop() error {
 	defer runtime.UnlockOSThread()
 	uid := os.Getuid()
