b16693a248fe4f52e9f52a2d68a41e5ac86a25b9
David Trudgian <david.trudgian@sylabs.io>
2022-09-22 15:25:57 +0100

    e2e: Re-work docker / pull related e2e tests (3.10)
    E2E tests using docker:// sources cannot run in parallel with caching,
    due to issues with concurrency and the containers/image oci/layout. We
    want to use caching as many tests rely on the same image, and Docker
    Hub rate limiting is still an issue where it is not trivial to provide
    credentials.
    
    This changeset:
    
    * Replaces some docker:// sources with equivalent library:// sources,
      in tests that are not explicitly for verifying docker specific
      behavior. library:// caching is concurrency safe on local fs with
      atomic operations (such as CI), allowing a speed-up.
    
    * Removes some redundant docker:// source tests from env/imgbuild
      etc. which are already covered by the docker group.
    
    * Moves all remaining docker:// source tests into the e2e.docker
      group, so that all tests using Docker Hub are isolated there. This
      could be used in future to disable them for e2e runs without Docker
      Hub credentials etc.
    
    * Runs the e2e.docker tests ordered sequentially, but parallel with
      respect to other non-docker tests.
    
    * Replaces a problematic Chdir operation in the non-docker pull tests
      with specifying the working directory on the exec.Command, so that
      these pull tests can also be run ordered sequentially, but parallel
      with respect to other non-pull tests.
    
    Speed-up reported on CircleCI from these changes is ~20%
    
    Fixes #977

--- e2e/imgbuild/regressions.go
+++ e2e/imgbuild/regressions.go
@@ -6,7 +6,6 @@
 package imgbuild
 
 import (
-	"bytes"
 	"io/ioutil"
 	"log"
 	"os"
@@ -17,7 +16,6 @@ import (
 
 	"github.com/google/uuid"
 	"github.com/sylabs/singularity/e2e/internal/e2e"
-	"github.com/sylabs/singularity/internal/pkg/test/tool/require"
 	"github.com/sylabs/singularity/internal/pkg/util/fs"
 )
 
@@ -111,50 +109,6 @@ func (c *imgBuildTests) issue4407(t *testing.T) {
 	}
 }
 
-// This test will build a sandbox, as a non-root user from a dockerhub image
-// that contains a single folder and file with `000` permission.
-// It will verify that with `--fix-perms` we force files to be accessible,
-// moveable, removable by the user. We check for `700` and `400` permissions on
-// the folder and file respectively.
-func (c *imgBuildTests) issue4524(t *testing.T) {
-	sandbox := filepath.Join(c.env.TestDir, "issue_4524")
-
-	c.env.RunSingularity(
-		t,
-		e2e.WithProfile(e2e.UserProfile),
-		e2e.WithCommand("build"),
-		e2e.WithArgs("--fix-perms", "--sandbox", sandbox, "docker://sylabsio/issue4524"),
-		e2e.PostRun(func(t *testing.T) {
-			// If we failed to build the sandbox completely, leave what we have for
-			// investigation.
-			if t.Failed() {
-				t.Logf("Test %s failed, not removing directory %s", t.Name(), sandbox)
-				return
-			}
-
-			if !e2e.PathPerms(t, path.Join(sandbox, "directory"), 0o700) {
-				t.Error("Expected 0700 permissions on 000 test directory in rootless sandbox")
-			}
-			if !e2e.PathPerms(t, path.Join(sandbox, "file"), 0o600) {
-				t.Error("Expected 0600 permissions on 000 test file in rootless sandbox")
-			}
-
-			// If the permissions aren't as we expect them to be, leave what we have for
-			// investigation.
-			if t.Failed() {
-				t.Logf("Test %s failed, not removing directory %s", t.Name(), sandbox)
-				return
-			}
-
-			err := os.RemoveAll(sandbox)
-			if err != nil {
-				t.Logf("Cannot remove sandbox directory: %#v", err)
-			}
-		}),
-		e2e.ExpectExit(0),
-	)
-}
-
 func (c *imgBuildTests) issue4583(t *testing.T) {
 	image := filepath.Join(c.env.TestDir, "issue_4583.sif")
 
@@ -199,22 +153,6 @@ func (c imgBuildTests) issue4837(t *testing.T) {
 				os.RemoveAll(filepath.Join(u.Dir, sandboxName))
 			}
 		}),
-		e2e.ExpectExit(0),
-	)
-}
-
-func (c *imgBuildTests) issue4943(t *testing.T) {
-	require.Arch(t, "amd64")
-
-	const (
-		image = "docker://gitlab-registry.cern.ch/linuxsupport/cc7-base:20191107"
-	)
-
-	c.env.RunSingularity(
-		t,
-		e2e.WithProfile(e2e.UserProfile),
-		e2e.WithCommand("build"),
-		e2e.WithArgs("--force", "/dev/null", image),
 		e2e.ExpectExit(0),
 	)
 }
@@ -304,56 +242,6 @@ func (c *imgBuildTests) issue5166(t *testing.T) {
 		e2e.PostRun(func(t *testing.T) {
 			if !t.Failed() {
 				cleanup(t)
-			}
-		}),
-		e2e.ExpectExit(0),
-	)
-}
-
-func (c *imgBuildTests) issue5172(t *testing.T) {
-	e2e.EnsureRegistry(t)
-
-	u := e2e.UserProfile.HostUser(t)
-
-	// create $HOME/.config/containers/registries.conf
-	regImage := "docker://localhost:5000/my-busybox"
-	regDir := filepath.Join(u.Dir, ".config", "containers")
-	regFile := filepath.Join(regDir, "registries.conf")
-	imagePath := filepath.Join(c.env.TestDir, "issue-5172")
-
-	if err := os.MkdirAll(regDir, 0o755); err != nil {
-		t.Fatalf("can't create directory %s: %s", regDir, err)
-	}
-
-	// add our test registry as insecure and test build/pull
-	b := new(bytes.Buffer)
-	b.WriteString("[registries.insecure]\nregistries = ['localhost']")
-	if err := ioutil.WriteFile(regFile, b.Bytes(), 0o644); err != nil {
-		t.Fatalf("can't create %s: %s", regFile, err)
-	}
-	defer os.RemoveAll(regDir)
-
-	c.env.RunSingularity(
-		t,
-		e2e.WithProfile(e2e.UserProfile),
-		e2e.WithCommand("build"),
-		e2e.WithArgs("--sandbox", imagePath, regImage),
-		e2e.PostRun(func(t *testing.T) {
-			if !t.Failed() {
-				os.RemoveAll(imagePath)
-			}
-		}),
-		e2e.ExpectExit(0),
-	)
-
-	c.env.RunSingularity(
-		t,
-		e2e.WithProfile(e2e.UserProfile),
-		e2e.WithCommand("pull"),
-		e2e.WithArgs(imagePath, regImage),
-		e2e.PostRun(func(t *testing.T) {
-			if !t.Failed() {
-				os.RemoveAll(imagePath)
 			}
 		}),
 		e2e.ExpectExit(0),
