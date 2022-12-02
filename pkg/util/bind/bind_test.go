b72a0bfa3ba21967015e3fd63c8189d91978b735
David Trudgian <EMAIL>
2022-12-02 12:20:50 +0000

    chore: move BindPath code out of runtime package

--- pkg/util/bind/bind_test.go
+++ pkg/util/bind/bind_test.go
@@ -1,9 +1,9 @@
-// Copyright (c) 2021, Sylabs Inc. All rights reserved.
+// Copyright (c) 2022, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
 
-package singularity
+package bind
 
 import (
 	"reflect"
