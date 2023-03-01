ddedd1fbc03e4256a50d0378d962ef13fd057c87
preminger <omer@preminger.xyz>
2023-03-01 09:00:52 -0500

    updated go-toml dependency to v2 (release-311)
    Pick #1383
    
    * updated go-toml dependency to v2
    
    * updated expected output for malformed TOML
    
    * updated go-toml dependency to v2
    
    * trying to clean up check-go-mod problems
    
    * added Omer Preminger
    
    * revert unintended dependency downgrades

--- internal/pkg/cgroups/config_linux.go
+++ internal/pkg/cgroups/config_linux.go
@@ -11,7 +11,7 @@ import (
 	"path/filepath"
 
 	specs "github.com/opencontainers/runtime-spec/specs-go"
-	"github.com/pelletier/go-toml"
+	"github.com/pelletier/go-toml/v2"
 )
 
 func Int64ptr(i int) *int64 {
