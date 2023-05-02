9b97d6583cbeb1de50a5984d447fff20aeea69a9
David Trudgian <david.trudgian@sylabs.io>
2023-05-02 10:34:26 +0100

    oci: Enable --writable-tmpfs behaviour by default
    The `--oci` mode intends to follow behaviour that the native runtime
    implements when run with `--compat`.
    
    One missing aspect is that `--compat` sets `--writable-tmpfs`, where
    the container rootfs is made writable with a tmpfs backed overlay.
    
    This PR:
    
    - Introduces a simple wrapping of the `oci run` sub-command as `oci
      run-wrapped`. This hidden command implements prep / cleanup steps
      that must take place in a userns for non-root `--oci` execution.
    - Switches the oci launcher to calling `oci run-wrapped` instead of
      `oci-run`.
    - Adds a tmpfs based overlay creation function for OCI
      bundles.
    - Includes the tmpfs overlay creation in the `oci run-wrapped` flow.
    - Copies the native runtime `--compat` e2e tests to OCI mode.
    
    Fixes #1621

--- internal/app/singularity/oci_linux.go
+++ internal/app/singularity/oci_linux.go
@@ -42,6 +42,15 @@ func OciRun(ctx context.Context, containerID string, args *OciArgs) error {
 	return oci.Run(ctx, containerID, args.BundlePath, args.PidFile, systemdCgroups)
 }
 
+// OciRun runs a container via the OCI runtime, wrapped with prep / cleanup steps.
+func OciRunWrapped(ctx context.Context, containerID string, args *OciArgs) error {
+	systemdCgroups, err := systemdCgroups()
+	if err != nil {
+		return err
+	}
+	return oci.RunWrapped(ctx, containerID, args.BundlePath, args.PidFile, systemdCgroups)
+}
+
 // OciCreate creates a container from an OCI bundle
 func OciCreate(containerID string, args *OciArgs) error {
 	systemdCgroups, err := systemdCgroups()
--- pkg/ocibundle/tools/overlay_linux.go
+++ pkg/ocibundle/tools/overlay_linux.go
@@ -12,7 +12,7 @@ import (
 	"syscall"
 )
 
-// CreateOverlay creates a writable overlay
+// CreateOverlay creates a writable overlay based on a directory.
 func CreateOverlay(bundlePath string) error {
 	var err error
 
@@ -45,18 +45,57 @@ func CreateOverlay(bundlePath string) error {
 		return fmt.Errorf("failed to remount %s: %s", overlayDir, err)
 	}
 
+	err = prepareOverlay(bundlePath, overlayDir)
+	return err
+}
+
+// CreateOverlay creates a writable overlay based on a tmpfs.
+func CreateOverlayTmpfs(bundlePath string, sizeMiB int) error {
+	var err error
+
+	oldumask := syscall.Umask(0)
+	defer syscall.Umask(oldumask)
+
+	overlayDir := filepath.Join(bundlePath, "overlay")
+	if err = os.Mkdir(overlayDir, 0o700); err != nil {
+		return fmt.Errorf("failed to create %s: %s", overlayDir, err)
+	}
+	// delete overlay directory in case of error
+	defer func() {
+		if err != nil {
+			os.RemoveAll(overlayDir)
+		}
+	}()
+
+	options := fmt.Sprintf("mode=1777,size=%dm", sizeMiB)
+	err = syscall.Mount("tmpfs", overlayDir, "tmpfs", syscall.MS_NODEV, options)
+	if err != nil {
+		return fmt.Errorf("failed to bind %s: %s", overlayDir, err)
+	}
+	// best effort to cleanup mount
+	defer func() {
+		if err != nil {
+			syscall.Unmount(overlayDir, syscall.MNT_DETACH)
+		}
+	}()
+
+	err = prepareOverlay(bundlePath, overlayDir)
+	return err
+}
+
+func prepareOverlay(bundlePath, overlayDir string) error {
 	upperDir := filepath.Join(overlayDir, "upper")
-	if err = os.Mkdir(upperDir, 0o755); err != nil {
+	if err := os.Mkdir(upperDir, 0o755); err != nil {
 		return fmt.Errorf("failed to create %s: %s", upperDir, err)
 	}
 	workDir := filepath.Join(overlayDir, "work")
-	if err = os.Mkdir(workDir, 0o700); err != nil {
+	if err := os.Mkdir(workDir, 0o700); err != nil {
 		return fmt.Errorf("failed to create %s: %s", workDir, err)
 	}
 	rootFsDir := RootFs(bundlePath).Path()
 
 	options := fmt.Sprintf("lowerdir=%s,upperdir=%s,workdir=%s", rootFsDir, upperDir, workDir)
-	if err = syscall.Mount("overlay", rootFsDir, "overlay", 0, options); err != nil {
+	if err := syscall.Mount("overlay", rootFsDir, "overlay", 0, options); err != nil {
 		return fmt.Errorf("failed to mount %s: %s", overlayDir, err)
 	}
 	return nil
