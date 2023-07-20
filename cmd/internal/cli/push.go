eec4330173eb58ec15cef3abd19cba173aa3d589
David Trudgian <david.trudgian@sylabs.io>
2023-07-20 10:30:18 +0100

    chore: Move library push impl to internal/pkg/client/library
    Implementation of pull from / push to for network sources is kept
    under internal/pkg/client/<source>.
    
    Prior to this PR, the implementation of push to a library://
    destination was an exception, with the code at
    internal/app/singularity
    
    Move and refactor that code, so it fits into the client/library
    package.
    
    This is necessary to keep the upcoming OCI registry and library
    OCI-SIF push implementation tidy.

--- cmd/internal/cli/push.go
+++ cmd/internal/cli/push.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2020, Control Command Inc. All rights reserved.
-// Copyright (c) 2018-2021, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2023, Sylabs Inc. All rights reserved.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
@@ -8,12 +8,15 @@ package cli
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
 	"github.com/sylabs/singularity/docs"
-	"github.com/sylabs/singularity/internal/app/singularity"
+	"github.com/sylabs/singularity/internal/pkg/client/library"
 	"github.com/sylabs/singularity/internal/pkg/client/oras"
 	"github.com/sylabs/singularity/internal/pkg/remote/endpoint"
+	"github.com/sylabs/singularity/internal/pkg/signature"
+	"github.com/sylabs/singularity/internal/pkg/util/fs"
 	"github.com/sylabs/singularity/internal/pkg/util/uri"
 	"github.com/sylabs/singularity/pkg/cmdline"
 	"github.com/sylabs/singularity/pkg/sylog"
@@ -89,6 +92,14 @@ var PushCmd = &cobra.Command{
 
 		switch transport {
 		case LibraryProtocol, "": // Handle pushing to a library
+			destRef, err := library.NormalizeLibraryRef(dest)
+			if err != nil {
+				sylog.Fatalf("Malformed library reference: %v", err)
+			}
+			if PushLibraryURI != "" && destRef.Host != "" {
+				sylog.Fatalf("Conflicting arguments; do not use --library with a library URI containing host name")
+			}
+
 			lc, err := getLibraryClientConfig(PushLibraryURI)
 			if err != nil {
 				sylog.Fatalf("Unable to get library client configuration: %v", err)
@@ -99,32 +110,49 @@ var PushCmd = &cobra.Command{
 				sylog.Fatalf("Cannot push image to library: %v", remoteWarning)
 			}
 
-			co, err := getKeyserverClientOpts("", endpoint.KeyserverVerifyOp)
+			if unsignedPush {
+				sylog.Warningf("Skipping container verification")
+			} else {
+				// Check if the container has a valid signature.
+				co, err := getKeyserverClientOpts("", endpoint.KeyserverVerifyOp)
+				if err != nil {
+					sylog.Fatalf("Unable to get keyserver client configuration: %v", err)
+				}
+				if err := signature.Verify(cmd.Context(), file, signature.OptVerifyWithPGP(co...)); err != nil {
+					fmt.Printf("TIP: You can push unsigned images with 'singularity push -U %s'.\n", file)
+					fmt.Printf("TIP: Learn how to sign your own containers by using 'singularity help sign'\n\n")
+					sylog.Fatalf("Unable to upload container: unable to verify signature")
+				}
+			}
+
+			resp, err := library.Push(cmd.Context(), file, destRef, pushDescription, lc)
 			if err != nil {
-				sylog.Fatalf("Unable to get keyserver client configuration: %v", err)
-			}
-
-			feURL, err := currentRemoteEndpoint.GetURL()
-			if err != nil {
-				sylog.Fatalf("Unable to find remote web URI %v", err)
-			}
-
-			pushSpec := singularity.LibraryPushSpec{
-				SourceFile:    file,
-				DestRef:       dest,
-				Description:   pushDescription,
-				AllowUnsigned: unsignedPush,
-				FrontendURI:   feURL,
-			}
-
-			err = singularity.LibraryPush(cmd.Context(), pushSpec, lc, co)
-			if err == singularity.ErrLibraryUnsigned {
-				fmt.Printf("TIP: You can push unsigned images with 'singularity push -U %s'.\n", file)
-				fmt.Printf("TIP: Learn how to sign your own containers by using 'singularity help sign'\n\n")
-				sylog.Fatalf("Unable to upload container: unable to verify signature")
-			} else if err != nil {
 				sylog.Fatalf("Unable to push image to library: %v", err)
 			}
+
+			// If the library supports direct upload into an OCI backing
+			// registry, then there is no response, and we are done.
+			if resp == nil {
+				return
+			}
+
+			// An older library may return a response with the container URL and
+			// quota information to display to the user.
+			used, quota := resp.Quota.QuotaUsageBytes, resp.Quota.QuotaTotalBytes
+			if quota == 0 {
+				fmt.Printf("\nLibrary storage: using %s out of unlimited quota\n", fs.FindSize(used))
+			} else {
+				fmt.Printf("\nLibrary storage: using %s out of %s quota (%.1f%% used)\n", fs.FindSize(used), fs.FindSize(quota), float64(used)/float64(quota)*100.0)
+			}
+			// If user didn't override the library URI we can show the URL to the container on the web for the current endpoint.
+			if PushLibraryURI == "" {
+				feURL, err := currentRemoteEndpoint.GetURL()
+				if err != nil {
+					sylog.Fatalf("Unable to find remote web URI %v", err)
+				}
+				fmt.Printf("Container URL: %s\n", feURL+"/"+strings.TrimPrefix(resp.ContainerURL, "/"))
+			}
+
 		case OrasProtocol:
 			if cmd.Flag(pushDescriptionFlag.Name).Changed {
 				sylog.Warningf("Description is not supported for push to oras. Ignoring it.")
