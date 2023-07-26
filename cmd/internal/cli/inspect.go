f2847a7b84785fa0bbd3d5e48e82877716b33b8e
Omer Preminger <omer@sylabs.io>
2023-07-26 11:09:58 -0400

    updated copyright headers

--- cmd/internal/cli/inspect.go
+++ cmd/internal/cli/inspect.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2018-2022, Sylabs Inc. All rights reserved.
+// Copyright (c) 2018-2023, Sylabs Inc. All rights reserved.
+// Copyright (c) Contributors to the Apptainer project, established as
+//   Apptainer a Series of LF Projects LLC.
 // This software is licensed under a 3-clause BSD license. Please consult the
 // LICENSE.md file distributed with the sources of this project regarding your
 // rights to use or distribute this software.
