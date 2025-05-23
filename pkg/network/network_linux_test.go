9e7df32925f77c99bf06659d1cfd160fff20ef39
David Trudgian <david.trudgian@sylabs.io>
2025-05-23 15:12:14 +0000

    fix: new lint

--- pkg/network/network_linux_test.go
+++ pkg/network/network_linux_test.go
@@ -8,7 +8,6 @@
 package network
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"net"
@@ -371,10 +370,10 @@ func testPingIP(nsPath string, cniPath *CNIPath, stdin io.WriteCloser, stdout io
 		return err
 	}
 	setup.SetArgs([]string{"IP=" + testIP})
-	if err := setup.AddNetworks(context.Background()); err != nil {
+	if err := setup.AddNetworks(t.Context()); err != nil {
 		return err
 	}
-	defer setup.DelNetworks(context.Background())
+	defer setup.DelNetworks(t.Context())
 
 	ip, err := setup.GetNetworkIP("test-bridge", "4")
 	if err != nil {
@@ -400,10 +399,10 @@ func testPingRandomIP(nsPath string, cniPath *CNIPath, stdin io.WriteCloser, std
 	if err != nil {
 		return err
 	}
-	if err := setup.AddNetworks(context.Background()); err != nil {
+	if err := setup.AddNetworks(t.Context()); err != nil {
 		return err
 	}
-	defer setup.DelNetworks(context.Background())
+	defer setup.DelNetworks(t.Context())
 
 	ip, err := setup.GetNetworkIP("test-bridge", "4")
 	if err != nil {
@@ -427,10 +426,10 @@ func testPingIPRange(nsPath string, cniPath *CNIPath, stdin io.WriteCloser, stdo
 		return err
 	}
 	setup.SetArgs([]string{"ipRange=10.111.112.0/24"})
-	if err := setup.AddNetworks(context.Background()); err != nil {
+	if err := setup.AddNetworks(t.Context()); err != nil {
 		return err
 	}
-	defer setup.DelNetworks(context.Background())
+	defer setup.DelNetworks(t.Context())
 
 	ip, err := setup.GetNetworkIP("test-bridge", "4")
 	if err != nil {
@@ -461,10 +460,10 @@ func testHTTPPortmap(nsPath string, cniPath *CNIPath, stdin io.WriteCloser, stdo
 		return err
 	}
 	setup.SetArgs([]string{"portmap=31080:80/tcp"})
-	if err := setup.AddNetworks(context.Background()); err != nil {
+	if err := setup.AddNetworks(t.Context()); err != nil {
 		return err
 	}
-	defer setup.DelNetworks(context.Background())
+	defer setup.DelNetworks(t.Context())
 
 	eth, err := setup.GetNetworkInterface("test-bridge-iprange")
 	if err != nil {
@@ -503,10 +502,10 @@ func testBadBridge(nsPath string, cniPath *CNIPath, stdin io.WriteCloser, stdout
 	if err != nil {
 		return err
 	}
-	if err := setup.AddNetworks(context.Background()); err == nil {
+	if err := setup.AddNetworks(t.Context()); err == nil {
 		return fmt.Errorf("unexpected success while calling non existent plugin")
 	}
-	defer setup.DelNetworks(context.Background())
+	defer setup.DelNetworks(t.Context())
 
 	return nil
 }
