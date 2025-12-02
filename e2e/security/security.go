apptainer/apptainer

e2e: improve apparmor / selinux tests
Signed-off-by: Dave Dykstra <2129743+DrDaveD@users.noreply.github.com>

e2e/security/security.go
@@ -12,6 +12,7 @@ package security
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/apptainer/apptainer/e2e/internal/e2e"
@@ -101,22 +102,6 @@ func (c ctx) testSecurityUnpriv(t *testing.T) {
 			argv:     []string{"grep", "^Cap.*:", "/proc/self/status"},
 			opts:     []string{"--drop-caps", "CAP_NET_RAW"},
 			expectOp: e2e.ExpectOutput(e2e.RegexMatch, `CapEff:\s+0+\n`),
-		},
-		// apparmor
-		{
-			name:     "apparmor",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "apparmor:/usr/bin/man"},
-			preFn:    require.Apparmor,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "/usr/bin/man (enforce)"),
-		},
-		// selinux
-		{
-			name:     "selinux",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
-			preFn:    require.Selinux,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "unconfined_u:unconfined_r:container_t:s0"),
 		},
 	}
 
@@ -214,22 +199,6 @@ func (c ctx) testSecurityPriv(t *testing.T) {
 			opts:     []string{"--drop-caps", "CAP_NET_RAW"},
 			expectOp: e2e.ExpectOutput(e2e.ContainMatch, dropCap),
 		},
-		// apparmor
-		{
-			name:     "apparmor",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "apparmor:/usr/bin/man"},
-			preFn:    require.Apparmor,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "/usr/bin/man (enforce)"),
-		},
-		// selinux
-		{
-			name:     "selinux",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
-			preFn:    require.Selinux,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "unconfined_u:unconfined_r:container_t:s0"),
-		},
 	}
 
 	e2e.EnsureImage(t, c.env)
@@ -287,6 +256,124 @@ func (c ctx) testSecurityConfOwnership(t *testing.T) {
 	)
 }
 
+// testApparmor tests the apparmor security flag.
+func (c ctx) testApparmor(t *testing.T) {
+	require.Apparmor(t)
+	e2e.EnsureImage(t, c.env)
+
+	tests := []struct {
+		name       string
+		image      string
+		argv       []string
+		opts       []string
+		expectOp   e2e.ApptainerCmdResultOp
+		expectExit int
+	}{
+		// apparmor
+		// Uses the profile for /usr/bin/man which will block `ls` in container root.
+		{
+			name:     "apparmor applied",
+			argv:     []string{"cat", "/proc/self/attr/current"},
+			opts:     []string{"--security", "apparmor:/usr/bin/man"},
+			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "/usr/bin/man (enforce)"),
+		},
+		{
+			name:       "apparmor denial",
+			argv:       []string{"ls", "/"},
+			opts:       []string{"--security", "apparmor:/usr/bin/man"},
+			expectExit: 1,
+			expectOp:   e2e.ExpectError(e2e.ContainMatch, "Permission denied"),
+		},
+	}
+
+	for _, profile := range e2e.Profiles {
+		t.Run(profile.String(), func(t *testing.T) {
+			for _, tt := range tests {
+				optArgs := []string{}
+				optArgs = append(optArgs, tt.opts...)
+				optArgs = append(optArgs, c.env.ImagePath)
+				optArgs = append(optArgs, tt.argv...)
+
+				c.env.RunApptainer(
+					t,
+					e2e.AsSubtest(tt.name),
+					e2e.WithProfile(profile),
+					e2e.WithCommand("exec"),
+					e2e.WithArgs(optArgs...),
+					e2e.ExpectExit(tt.expectExit, tt.expectOp),
+				)
+			}
+		})
+	}
+}
+
+// testSELinux tests the selinux security flag.
+func (c ctx) testSELinux(t *testing.T) {
+	require.Selinux(t)
+	require.Command(t, "chcon")
+	e2e.EnsureImage(t, c.env)
+
+	sandbox, cleanup := e2e.MakeTempDir(t, c.env.TestDir, "sandbox-", "")
+	defer cleanup(t)
+	// convert test image to sandbox
+	c.env.RunApptainer(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithCommand("build"),
+		e2e.WithArgs("--force", "--sandbox", sandbox, c.env.ImagePath),
+		e2e.ExpectExit(0),
+	)
+	// label as `container_ro_t`
+	cmd := exec.Command("chcon", "-R", "-t", "container_ro_file_t", sandbox)
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("while labeling sandbox: %v", err)
+	}
+
+	tests := []struct {
+		name       string
+		image      string
+		argv       []string
+		opts       []string
+		expectOp   e2e.ApptainerCmdResultOp
+		expectExit int
+	}{
+		// selinux
+		// Uses the container_t label which will block `ls` in /tmp bind from host.
+		{
+			name:     "selinux applied",
+			argv:     []string{"cat", "/proc/self/attr/current"},
+			opts:     []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
+			expectOp: e2e.ExpectOutput(e2e.ContainMatch, "unconfined_u:unconfined_r:container_t:s0"),
+		},
+		{
+			name:       "selinux denial",
+			argv:       []string{"ls", "/tmp"},
+			opts:       []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
+			expectExit: 1,
+			expectOp:   e2e.ExpectError(e2e.ContainMatch, "Permission denied"),
+		},
+	}
+	for _, profile := range e2e.Profiles {
+		t.Run(profile.String(), func(t *testing.T) {
+			for _, tt := range tests {
+				optArgs := []string{}
+				optArgs = append(optArgs, tt.opts...)
+				optArgs = append(optArgs, sandbox)
+				optArgs = append(optArgs, tt.argv...)
+
+				c.env.RunApptainer(
+					t,
+					e2e.AsSubtest(tt.name),
+					e2e.WithProfile(profile),
+					e2e.WithCommand("exec"),
+					e2e.WithArgs(optArgs...),
+					e2e.ExpectExit(tt.expectExit, tt.expectOp),
+				)
+			}
+		})
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := ctx{
@@ -299,5 +386,7 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"apptainerSecurityUnpriv":   c.testSecurityUnpriv,
 		"apptainerSecurityPriv":     c.testSecurityPriv,
 		"testSecurityConfOwnership": np(c.testSecurityConfOwnership),
+		"testApparmor":              c.testApparmor,
+		"testSELinux":               c.testSELinux,
 	}
 }

sylabs/singularity

e2e/security/security.go
@@ -8,6 +8,7 @@ package security
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"testing"
 
 	"github.com/sylabs/singularity/v4/e2e/internal/e2e"
@@ -84,22 +85,6 @@ func (c ctx) testSecurityUnpriv(t *testing.T) {
 			argv:     []string{"grep", "^Cap.*:", "/proc/self/status"},
 			opts:     []string{"--drop-caps", "CAP_NET_RAW"},
 			expectOp: e2e.ExpectOutput(e2e.RegexMatch, `CapEff:\s+0+\n`),
-		},
-		// apparmor
-		{
-			name:     "apparmor",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "apparmor:/usr/bin/man"},
-			preFn:    require.Apparmor,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "/usr/bin/man (enforce)"),
-		},
-		// selinux
-		{
-			name:     "selinux",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
-			preFn:    require.Selinux,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "unconfined_u:unconfined_r:container_t:s0"),
 		},
 	}
 
@@ -191,22 +176,6 @@ func (c ctx) testSecurityPriv(t *testing.T) {
 			opts:     []string{"--drop-caps", "CAP_NET_RAW"},
 			expectOp: e2e.ExpectOutput(e2e.ContainMatch, dropCap),
 		},
-		// apparmor
-		{
-			name:     "apparmor",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "apparmor:/usr/bin/man"},
-			preFn:    require.Apparmor,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "/usr/bin/man (enforce)"),
-		},
-		// selinux
-		{
-			name:     "selinux",
-			argv:     []string{"cat", "/proc/self/attr/current"},
-			opts:     []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
-			preFn:    require.Selinux,
-			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "unconfined_u:unconfined_r:container_t:s0"),
-		},
 	}
 
 	e2e.EnsureImage(t, c.env)
@@ -263,6 +232,124 @@ func (c ctx) testSecurityConfOwnership(t *testing.T) {
 	)
 }
 
+// testApparmor tests the apparmor security flag.
+func (c ctx) testApparmor(t *testing.T) {
+	require.Apparmor(t)
+	e2e.EnsureImage(t, c.env)
+
+	tests := []struct {
+		name       string
+		image      string
+		argv       []string
+		opts       []string
+		expectOp   e2e.SingularityCmdResultOp
+		expectExit int
+	}{
+		// apparmor
+		// Uses the profile for /usr/bin/man which will block `ls` in container root.
+		{
+			name:     "apparmor applied",
+			argv:     []string{"cat", "/proc/self/attr/current"},
+			opts:     []string{"--security", "apparmor:/usr/bin/man"},
+			expectOp: e2e.ExpectOutput(e2e.ExactMatch, "/usr/bin/man (enforce)"),
+		},
+		{
+			name:       "apparmor denial",
+			argv:       []string{"ls", "/"},
+			opts:       []string{"--security", "apparmor:/usr/bin/man"},
+			expectExit: 1,
+			expectOp:   e2e.ExpectError(e2e.ContainMatch, "Permission denied"),
+		},
+	}
+
+	for _, profile := range e2e.NativeProfiles {
+		t.Run(profile.String(), func(t *testing.T) {
+			for _, tt := range tests {
+				optArgs := []string{}
+				optArgs = append(optArgs, tt.opts...)
+				optArgs = append(optArgs, c.env.ImagePath)
+				optArgs = append(optArgs, tt.argv...)
+
+				c.env.RunSingularity(
+					t,
+					e2e.AsSubtest(tt.name),
+					e2e.WithProfile(profile),
+					e2e.WithCommand("exec"),
+					e2e.WithArgs(optArgs...),
+					e2e.ExpectExit(tt.expectExit, tt.expectOp),
+				)
+			}
+		})
+	}
+}
+
+// testSELinux tests the selinux security flag.
+func (c ctx) testSELinux(t *testing.T) {
+	require.Selinux(t)
+	require.Command(t, "chcon")
+	e2e.EnsureImage(t, c.env)
+
+	sandbox, cleanup := e2e.MakeTempDir(t, c.env.TestDir, "sandbox-", "")
+	defer cleanup(t)
+	// convert test image to sandbox
+	c.env.RunSingularity(
+		t,
+		e2e.WithProfile(e2e.UserProfile),
+		e2e.WithCommand("build"),
+		e2e.WithArgs("--force", "--sandbox", sandbox, c.env.ImagePath),
+		e2e.ExpectExit(0),
+	)
+	// label as `container_ro_t`
+	cmd := exec.Command("chcon", "-R", "-t", "container_ro_file_t", sandbox)
+	if err := cmd.Run(); err != nil {
+		t.Fatalf("while labeling sandbox: %v", err)
+	}
+
+	tests := []struct {
+		name       string
+		image      string
+		argv       []string
+		opts       []string
+		expectOp   e2e.SingularityCmdResultOp
+		expectExit int
+	}{
+		// selinux
+		// Uses the container_t label which will block `ls` in /tmp bind from host.
+		{
+			name:     "selinux applied",
+			argv:     []string{"cat", "/proc/self/attr/current"},
+			opts:     []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
+			expectOp: e2e.ExpectOutput(e2e.ContainMatch, "unconfined_u:unconfined_r:container_t:s0"),
+		},
+		{
+			name:       "selinux denial",
+			argv:       []string{"ls", "/tmp"},
+			opts:       []string{"--security", "selinux:unconfined_u:unconfined_r:container_t:s0"},
+			expectExit: 1,
+			expectOp:   e2e.ExpectError(e2e.ContainMatch, "Permission denied"),
+		},
+	}
+	for _, profile := range e2e.NativeProfiles {
+		t.Run(profile.String(), func(t *testing.T) {
+			for _, tt := range tests {
+				optArgs := []string{}
+				optArgs = append(optArgs, tt.opts...)
+				optArgs = append(optArgs, sandbox)
+				optArgs = append(optArgs, tt.argv...)
+
+				c.env.RunSingularity(
+					t,
+					e2e.AsSubtest(tt.name),
+					e2e.WithProfile(profile),
+					e2e.WithCommand("exec"),
+					e2e.WithArgs(optArgs...),
+					e2e.ExpectExit(tt.expectExit, tt.expectOp),
+				)
+			}
+		})
+	}
+}
+
 // E2ETests is the main func to trigger the test suite
 func E2ETests(env e2e.TestEnv) testhelper.Tests {
 	c := ctx{
@@ -275,6 +362,8 @@ func E2ETests(env e2e.TestEnv) testhelper.Tests {
 		"singularitySecurityUnpriv": c.testSecurityUnpriv,
 		"singularitySecurityPriv":   c.testSecurityPriv,
 		"testSecurityConfOwnership": np(c.testSecurityConfOwnership),
+		"testApparmor":              c.testApparmor,
+		"testSELinux":               c.testSELinux,
 		// OCI-Mode
 		"ociCapabilities": c.ociCapabilities,
 	}